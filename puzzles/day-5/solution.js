ed0b2da1858d68d3e04c17eba6b222b57066d5d1
Nick Arocho <nicaroch@amazon.com>
2021-12-10 12:34:41 -0800

    day 6, step 1

--- puzzles/day-5/solution.js
+++ puzzles/day-5/solution.js
@@ -1,57 +1,30 @@
 const fs = require("fs");
-const read = fs.readFileSync("puzzles/day-5/segment-data.txt");
-let data = read
-  .toString()
-  .split("\n")
-  .map((row) => {
-    return row.split(" -> ").map((pos) => pos.split(",").map(Number));
-  });
+const read = fs.readFileSync("puzzles/day-6/fish-age-data.txt");
+let data = read.toString().split(",").map(Number);
 
-function findOverlaps(data) {
-  let map = {};
-  for (let [[startX, startY], [endX, endY]] of data) {
-    const xCoords = [Math.min(startX, endX), Math.max(startX, endX)];
-    const yCoords = [Math.min(startY, endY), Math.max(startY, endY)];
+function projectFishCount(data, days) {
+  let schoolOfFish = [...data];
 
-    if (startX === endX || startY === endY) {
-      // horizontal && vertical lines
-      for (let x = xCoords[0]; x <= xCoords[1]; x++) {
-        for (let y = yCoords[0]; y <= yCoords[1]; y++) {
-          const key = `${x}-${y}`;
-          map[key] = map[key] ? (map[key] += 1) : 1;
-        }
+  function ageFish(ages) {
+    let newFish = [];
+    const newAges = ages.map((age) => {
+      if (age === 0) {
+        newFish.push(8);
+        return 6;
+      } else {
+        return age - 1;
       }
-    } else {
-      // diagonal lines
-      // flips "backwards" coordinates... helps me think about these all the same way, though it shouldn't matter
-      const [firstPoint, secondPoint] =
-        startX < endX
-          ? [
-              [startX, startY],
-              [endX, endY],
-            ]
-          : [
-              [endX, endY],
-              [startX, startY],
-            ];
-      const direction = firstPoint[1] > secondPoint[1] ? "up" : "down";
-
-      let y = firstPoint[1];
-      for (let x = xCoords[0]; x <= xCoords[1]; x++) {
-        const key = `${x}-${y}`;
-        y = direction === "down" ? (y += 1) : (y -= 1);
-        map[key] = map[key] ? (map[key] += 1) : 1;
-      }
-    }
+    });
+    schoolOfFish = [...newAges, ...newFish];
+    return schoolOfFish;
   }
 
-  let overlaps = 0;
-  Object.values(map).forEach((val) => {
-    if (val > 1) overlaps += 1;
-  });
+  for (let i = 0; i < days; i++) {
+    ageFish(schoolOfFish);
+  }
 
-  return overlaps;
+  return schoolOfFish.length;
 }
 
-const overlaps = findOverlaps(data);
-console.log({ overlaps });
+const result = projectFishCount(data, 80);
+console.log({ result });
