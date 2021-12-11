b1cbbf01055323a7ea91f9c862c2c9de8a9cb1f3
Nick Arocho <nicaroch@amazon.com>
2021-12-11 11:04:46 -0800

    day 6, step 2

--- puzzles/day-5/solution.js
+++ puzzles/day-5/solution.js
@@ -3,28 +3,34 @@ const read = fs.readFileSync("puzzles/day-6/fish-age-data.txt");
 let data = read.toString().split(",").map(Number);
 
 function projectFishCount(data, days) {
-  let schoolOfFish = [...data];
+  const schoolOfFish = {};
+  for (let i = 0; i < 9; i++) {
+    schoolOfFish[i] = 0;
+  }
 
-  function ageFish(ages) {
-    let newFish = [];
-    const newAges = ages.map((age) => {
-      if (age === 0) {
-        newFish.push(8);
-        return 6;
-      } else {
-        return age - 1;
-      }
-    });
-    schoolOfFish = [...newAges, ...newFish];
-    return schoolOfFish;
+  data.forEach((fishTimer) => {
+    schoolOfFish[fishTimer]++;
+  });
+
+  function ageFish(fish) {
+    // store this value before it shifts
+    const todaysZeros = fish[0];
+    for (let i = 0; i < 8; i++) {
+      // adds a day by shifting count from yesterday
+      fish[i] = fish[i + 1];
+    }
+    // today's new fish === one for every '0' from yesterday
+    fish[8] = todaysZeros;
+    // reset the cycle for the fish to reproduce again after 6 days
+    fish[6] += todaysZeros;
   }
 
   for (let i = 0; i < days; i++) {
     ageFish(schoolOfFish);
   }
 
-  return schoolOfFish.length;
+  return Object.values(schoolOfFish).reduce((acc, cur) => acc + cur, 0);
 }
 
-const result = projectFishCount(data, 80);
+const result = projectFishCount(data, 256);
 console.log({ result });
